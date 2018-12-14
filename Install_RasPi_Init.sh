6d74058b5a0c0088780f829b1606b62fb474d747
Liwei0503 <liwei0503@gmail.com>
2018-12-14 22:07:34 +0800

    Update Install_RasPi_Init.sh

--- Install_RasPi_Init.sh
+++ Install_RasPi_Init.sh
@@ -50,16 +50,3 @@ workon cv
 
 pip install numpy
 
-cd ~/opencv
-mkdir build
-cd build
-
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-    -D ENABLE_NEON=ON \
-    -D ENABLE_VFPV3=ON \
-    -D BUILD_TESTS=OFF \
-    -D OPENCV_ENABLE_NONFREE=ON \
-    -D INSTALL_PYTHON_EXAMPLES=OFF \
-    -D BUILD_EXAMPLES=OFF ..
